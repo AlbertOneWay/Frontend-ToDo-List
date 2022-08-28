1c0aeb978ba78a016f2596b7276e2276440fdb75
AlbertMunoz <albertmunoz131@gmail.com>
2022-08-27 21:00:11 -0500

    Creación de componentes de la interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './public/template/header/header.component';
+import { FooterComponent } from './public/template/footer/footer.component';
+import { NotFoundComponent } from './public/errors/not-found/not-found.component';
+import { InternalServerErrorComponent } from './public/errors/internal-server-error/internal-server-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    InternalServerErrorComponent
   ],
   imports: [
     BrowserModule,
